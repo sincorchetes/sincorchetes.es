c558a78c2f178328b0adee2bfce1614f4835ebc8
Álvaro Castillo <sincorchetes@gmail.com>
2026-04-18 04:47:37 +0100

    Init repo

---
+++ site/astro.config.mjs
@@ -0,0 +1,21 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://sincorchetes.es',
+  integrations: [
+    react(),
+    sitemap()
+  ],
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'en'],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  }
+});
